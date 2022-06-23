9b9b67e106f122077034f9fbce6e64692a95b3a1
EuniceNam <caellime@gmail.com>
2022-06-23 09:28:57 +0900

    Create Xcode proj

---
+++ CCC-1st-AppStore-Eunice/CCC-1st-AppStore-Eunice/CCC_1st_AppStore_EuniceApp.swift
@@ -0,0 +1,17 @@
+//
+//  CCC_1st_AppStore_EuniceApp.swift
+//  CCC-1st-AppStore-Eunice
+//
+//  Created by Hyorim Nam on 2022/06/23.
+//
+
+import SwiftUI
+
+@main
+struct CCC_1st_AppStore_EuniceApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ CCC-1st-AppStore-Eunice/CCC-1st-AppStore-Eunice/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  CCC-1st-AppStore-Eunice
+//
+//  Created by Hyorim Nam on 2022/06/23.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
